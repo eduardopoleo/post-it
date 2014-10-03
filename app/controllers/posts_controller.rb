7af158ca9b9860f72398a1caab5e839f977b7c63
Eduardo Poleo <eduardopoleo@gmail.com>
2014-10-02 22:28:55 -0400

    Add new actions and validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,67 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def show
+    # params is a hash where all the request info is stored.
+    # So it basically takes all the info from one side of the request to the other
+    @post = Post.find(params[:id])
+  end
+
+  def new
+
+    # this post has to be defined only if we are using 
+    # the backend -forms because we need to pass it to
+    # the form so that it knows what object are we talking about
+    @post = Post.new
+  end
+
+  def create
+    # Pure HTML & regular helpers
+
+    # @post= Post.new
+    # @post.title = params[:title]
+    # @post.url = params[:url]
+    # @post.description = params[:description]
+    # @post.save
+ 
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Post successfully created"
+      redirect_to post_path(@post)
+    else
+      # render does not count as new request
+      # so @post.errors stored in here are passed to the new view
+    render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      flash[:notice] = "Post successfully updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  # here starts the strong params definition.
+  private
+
+  def post_params
+    # Strong parameters are there just to regulate what parameter are mass updated
+    # Require is tells the data base what params are needed 
+    # Permit tells which are allowed for mass updating
+    # The constructions params.require(:post) assumes a nested structure
+    params.require(:post).permit! 
 
   end
 end
